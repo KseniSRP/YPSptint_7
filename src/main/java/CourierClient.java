9bea0e5f093aef58e30b9b5e2b8045c754183d18
Kseniia Seriapina <mimi.kseni@gmail.com>
2024-02-14 10:20:53 +0000

    CourierClient и OrdersClient в src.main.java также была изменена область видимости io.rest-assured для работы в основном коде

--- src/main/java/CourierClient.java
+++ src/main/java/CourierClient.java
@@ -1,7 +1,7 @@
 import io.qameta.allure.Step;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import io.restassured.RestAssured;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 
 public class CourierClient {
 
@@ -9,100 +9,100 @@ public class CourierClient {
 
     @Step("Создание курьера")
     public static void createCourier(Courier courier) {
-        given()
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .body(courier)
                 .when()
                 .post("/api/v1/courier")
                 .then()
                 .statusCode(201)
-                .body("ok", equalTo(true));
+                .body("ok", CoreMatchers.equalTo(true));
     }
 
 
     @Step("Логин курьера")
     public static void loginCourier(String login, String password) {
-        given()
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .body("{\"login\":\"" + login + "\", \"password\":\"" + password + "\"}")
                 .when()
                 .post("/api/v1/courier/login")
                 .then()
                 .statusCode(200)
-                .body("id", notNullValue());
+                .body("id", Matchers.notNullValue());
 
     }
 
     @Step("Удаление курьера")
     public static void deleteCourier(int courierId) {
-        given()
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .pathParam("id", courierId)
                 .when()
                 .delete("/api/v1/courier/{id}")
                 .then()
                 .statusCode(200)
-                .body("ok", equalTo(true));
+                .body("ok", CoreMatchers.equalTo(true));
     }
 
     @Step("Попытка создать дубликат учетной записи курьера")
     public static void attemptToCreateDuplicateCourier(Courier courier) {
-        given()
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .body(courier)
                 .when()
                 .post("/api/v1/courier")
                 .then()
                 .statusCode(409) // Ожидается конфликт из-за дублирования
-                .body("message", equalTo("Этот логин уже используется. Попробуйте другой."));
+                .body("message", CoreMatchers.equalTo("Этот логин уже используется. Попробуйте другой."));
     }
 
     @Step("Попытка создать курьера с невалидными данными")
     public static void attemptToCreateCourierWithInvalidData(Courier courier) {
-        given()
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .body(courier)
                 .when()
                 .post("/api/v1/courier")
                 .then()
                 .statusCode(400)
-                .body("message", equalTo("Недостаточно данных для создания учетной записи"));
+                .body("message", CoreMatchers.equalTo("Недостаточно данных для создания учетной записи"));
     }
 
     @Step("Попытка входа в систему с невалидными учетными данными {login}/{password}")
     public static void loginWithInvalidCredentials(String login, String password) {
-        given()
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .body("{\"login\":\"" + login + "\", \"password\":\"" + password + "\"}")
                 .when()
                 .post("/api/v1/courier/login")
                 .then()
                 .statusCode(404)
-                .body("message", equalTo("Учетная запись не найдена"));
+                .body("message", CoreMatchers.equalTo("Учетная запись не найдена"));
     }
 
     @Step("Попытка логина без учетных данных")
     public static void loginWithoutCredentials() {
-        given()
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .body("{\"login\":\"\", \"password\":\"\"}")
                 .when()
                 .post("/api/v1/courier/login")
                 .then()
                 .statusCode(400)
-                .body("message", equalTo("Недостаточно данных для входа"));
+                .body("message", CoreMatchers.equalTo("Недостаточно данных для входа"));
     }
 
     @Step("Логин курьера с логином {login} и паролем {password}")
     public static void courierLogin(String login, String password) {
-        given()
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .body("{\"login\":\"" + login + "\", \"password\":\"" + password + "\"}")
                 .when()
                 .post("/api/v1/courier/login")
                 .then()
                 .statusCode(200)
-                .body("id", notNullValue());
+                .body("id", Matchers.notNullValue());
     }
 
 }
--- src/main/java/OrdersClient.java
+++ src/main/java/OrdersClient.java
@@ -1,33 +1,32 @@
 import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class OrdersClient {
 
     @Step("Создание заказа с цветом: {0}")
     public static void createOrder(Order order){
-        given()
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .body(order)
                 .when()
                 .post("/api/v1/orders")
                 .then()
                 .statusCode(201)
-                .body("track", notNullValue());
+                .body("track", Matchers.notNullValue());
     }
 
     @Step("Получение списка всех заказов без указания courierId")
     public static void ordersListGet() {
-        given()
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .when()
                 .get("/api/v1/orders")
                 .then()
                 .statusCode(200) // Проверка статуса кода 200
-                .body("orders", notNullValue()) // Проверка на наличие массива заказов
-                .body("orders.size()", greaterThan(0)); // Проверка, что в списке есть заказы
+                .body("orders", Matchers.notNullValue()) // Проверка на наличие массива заказов
+                .body("orders.size()", Matchers.greaterThan(0)); // Проверка, что в списке есть заказы
 
     }
 }
